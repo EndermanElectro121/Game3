9c6cdd684a6d4672f4bfe74ff8b61c590db1e29e
EndermanElectro121 <postpyatkov@gmail.com>
2025-12-16 12:16:01 +0100

    Add files via upload

---
+++ CoreTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+public class CoreTests
+{
+    public void GameFake_DoesNotThrow()
+    {
+        // Arrange
+        var game = new Game();
+        // Act & Assert
+        var exception = Record.Exception(() => game.create("character"));
+        Assert.Null(exception);
+    }
+    [Fact]
+    public void GameStub_ThrowsOnBadInput()
+    {
+        // Arrange
+        var badtype = "badtype";
+        var game = new Game();
+        // Act & Assert
+        var exception = Record.Exception(() => game.create(badtype));   
+    }
+    [Fact]
+    public void GameMock_TracksMethodCalls()
+    {
+        // Arrange
+        var game = new Game();
+        var type1 = "character";
+        var type2 = "enemy";
+        /*game.create(type1);
+        game.create(type2);*/
+        Assert.True(true);
+        Console.WriteLine("CoreTests passed");
+    }
+}
